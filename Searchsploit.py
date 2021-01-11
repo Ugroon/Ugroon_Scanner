206d569e4b22d526d453c53706eb3641055b5686
Ugroon <77273551+Ugroon@users.noreply.github.com>
2021-01-11 15:24:22 +0300

    Add files via upload

---
+++ Searchsploit.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+
+os.system("apt update")
+os.system("apt full-upgrade")
+os.system("apt install figlet")
+os.system("apt full upgrade figlet")
+os.system("apt install searchsploit")
+os.system("apt full-upgrade searchsploit")
+os.system("clear")
+os.system("figlet WELCOME TO SEARCHSPLOİT")
+
+print("""
+
+Searchsploit aracına hoşgeldiniz.
+
+""")
+
+
+aranan = input("Aranacak exploit: ")
+
+os.system("searchsploit " + aranan)
+
+soru = input("Yeni arama yapmak istermisiniz? (e/h): ")
+
+if soru == "E" or "e":
+    os.system("python3 Searchsploit.py")
+
+else:
+    print("Programdan çıkılıyor.")
+
+    import time
+    time.sleep(3)
+
+    exit()
+    
