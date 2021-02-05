880bffdee57460f6196553b1893e4f95696fa7f0
Ugroon <77273551+Ugroon@users.noreply.github.com>
2021-02-05 20:31:25 +0300

    Update wafw00f.py

--- wafw00f.py
+++ wafw00f.py
@@ -3,6 +3,8 @@
 
 import os
 
+import time
+
 os.system("apt install wafw00f && apt upgrade wafw00f")
 
 os.system("clear")
@@ -10,29 +12,55 @@ os.system("clear")
 os.system("figlet Guvenlik Duvari Tespit")
 
 print("""
+1) Bulunabilinen güvenlik duvarları
+
+2) Güvenlik duvarı bulma
+
+
+""")
+
+secim = input("Seçenek: ")
+
+if secim == "1":
+    
+    os.system("wafw00f -l")
+    
+    
+elif secim == "2":
+    
+    print("""
 
 Güvenlik duvarı tespit programına hoş geldiniz.
 
 """)
 
-hedef = input("Hedef site: ")
+    hedef = input("Hedef site: ")
 
-os.system("clear")
+    os.system("clear")
 
-os.system("wafw00f " + hedef)
+    os.system("wafw00f " + hedef)
 
-tekrar = input("Programı tekrar başlatmak istermisiniz? (E/h): ")
+    tekrar = input("Programı tekrar başlatmak istermisiniz? (E/h): ")
 
-e = str("E")
+    e = str("E")
 
-if e == tekrar.upper():
+    if e == tekrar.upper():
 
-    os.system("python3 wafw00f.py")
+        os.system("python3 wafw00f.py")
     
-elif tekrar == "":
+    elif tekrar == "":
     
-    os.system("python3 wafw00f.py")
+        os.system("python3 wafw00f.py")
 
+    else:
+
+        exit()
+        
 else:
-
+    
+    print("Hatalı seçim yaptığınız için program kapatılıyor...")
+    
+    time.sleep(3)
+    
     exit()
+    
